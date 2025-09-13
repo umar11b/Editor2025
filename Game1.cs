ed1adc035f2b2d5d524e2c6d24588a28c3031a25
umar11b <uzaman2018@gmail.com>
2025-09-12 20:53:39 -0400

    Initial commit: MonoGame Editor 2025 starter project code

---
+++ Game1.cs
@@ -0,0 +1,50 @@
+﻿using System.Data.Common;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Editor2025;
+
+public class Game1 : Game
+{
+    private GraphicsDeviceManager _graphics;
+    private SpriteBatch _spriteBatch;
+    private Model _model;
+
+    public Game1()
+    {
+        _graphics = new GraphicsDeviceManager(this);
+        Content.RootDirectory = "Content";
+        IsMouseVisible = true;
+    }
+    protected override void Initialize()
+    {
+        // TODO: Add your initialization logic here
+        base.Initialize();
+    }
+    protected override void LoadContent()
+    {
+        _spriteBatch = new SpriteBatch(GraphicsDevice);
+        _model = Content.Load<Model>("Teapot");
+        // TODO: use this.Content to load your game content here
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            Exit();
+        // TODO: Add your update logic here
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(Color.Black);
+        // TODO: Add your drawing code here
+        foreach (var mesh in _model.Meshes)
+        {
+            mesh.Draw();
+        }
+        base.Draw(gameTime);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,2 @@
+﻿using var game = new Editor2025.Game1();
+game.Run();
